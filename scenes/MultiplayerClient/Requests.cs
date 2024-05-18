68241d62bf3823b65aec84e9fc03c0dab90c701a
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2024-05-18 20:38:24 +0400

    requests

--- scenes/MultiplayerClient/Requests.cs
+++ scenes/MultiplayerClient/Requests.cs
@@ -1,6 +1,20 @@
 using Godot;
+using MySqlConnector;
 
-partial class Requests
+partial class MultiplayerClient
 {
+    public void CreateMatch(int firstPlayerId, int secondPlayerId)
+    {
+        var query = @$"
+        INSERT INTO cydecks_db.matches (`finished`) VALUES (0);
+        SET @last_id = LAST_INSERT_ID();
+        INSERT INTO cydecks_db.match_players (`match_id`, `user_id`, `score`) 
+        VALUES (@last_id, {firstPlayerId}, 0), (@last_id, {secondPlayerId}, 0);
+        ";
+
+        var command = new MySqlCommand(query, dbConnection.Connection);
+        command.ExecuteNonQuery();
+    }
+
     
 }
